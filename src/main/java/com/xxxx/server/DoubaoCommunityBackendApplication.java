c01111f252da56774a48e8bcf84ec9e3ba3a3173
liuke <liukechn@163.com>
2022-05-22 00:35:07 +0800

    首次提交，gitignore了部分文件

---
+++ src/main/java/com/xxxx/server/DoubaoCommunityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xxxx.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubaoCommunityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoubaoCommunityBackendApplication.class, args);
+    }
+
+}
