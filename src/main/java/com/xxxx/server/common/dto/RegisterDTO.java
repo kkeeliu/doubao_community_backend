b3b3faf09fbece72473afbb4bb1d652d607859f5
liuke <liukechn@163.com>
2022-07-24 08:58:02 +0800

    RegisterDTO添加@ApiModelProperty()注解

--- src/main/java/com/xxxx/server/common/dto/RegisterDTO.java
+++ src/main/java/com/xxxx/server/common/dto/RegisterDTO.java
@@ -1,5 +1,6 @@
 package com.xxxx.server.common.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
@@ -17,17 +18,21 @@ public class RegisterDTO {
 
     @NotEmpty(message = "请输入账号")
     @Length(min = 2, max = 15, message = "长度在2-15")
+    @ApiModelProperty(value = "账号",required = true)
     private String name;
 
     @NotEmpty(message = "请输入密码")
     @Length(min = 6, max = 20, message = "长度在6-20")
+    @ApiModelProperty(value = "密码",required = true)
     private String pass;
 
     @NotEmpty(message = "请再次输入密码")
     @Length(min = 6, max = 20, message = "长度在6-20")
+    @ApiModelProperty(value = "二次密码",required = true)
     private String checkPass;
 
     @NotEmpty(message = "请输入电子邮箱")
     @Email(message = "邮箱格式不正确")
+    @ApiModelProperty(value = "电子邮箱",required = true)
     private String email;
 }
